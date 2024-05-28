853070db4cdfd9a140cc80ae78845924fa28ef43
Vigor Lundkvist <vigor.lundkvist@live.se>
2024-05-28 11:26:17 +0200

    Initial commit

---
+++ src/main/java/com/example/restaurant_webpage/RestaurantWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWebpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurant_webpage/RestaurantWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
